ac54140dd65b1467e6068f3036ff718e7820a368
ahmet bulutluoz <abbulutluoz@gmail.com>
2021-06-30 23:27:43 +0300

    4.gun sonu

--- src/day04_increment_matematiksel_islemler/C3_PreDecrement_PostDecrement.java
+++ src/day04_increment_matematiksel_islemler/C3_PreDecrement_PostDecrement.java
@@ -1,5 +1,28 @@
 package day04_increment_matematiksel_islemler;
 
 public class C3_PreDecrement_PostDecrement {
+	
+	public static void main(String[] args) {
+		
+		
+		int sayi1=20;
+		
+		int sayi2 = sayi1--;
+		
+		System.out.println("post decrement sayi1 : " + sayi1); // 
+		System.out.println("post decrement sayi2 : " + sayi2); // 
+		
+		
+		System.out.println(--sayi2); // 
+		
+		System.out.println(sayi2--); // 
+		
+		System.out.println(sayi2);  // 
+		
+		
+		
+		
+		
+	}
 
 }
