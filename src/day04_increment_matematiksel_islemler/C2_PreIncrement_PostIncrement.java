377d2cb0ab729e663400b4e273309deaa1f05550
ahmet bulutluoz <abbulutluoz@gmail.com>
2021-06-30 22:32:58 +0300

    4.gun ara

---
+++ src/day04_increment_matematiksel_islemler/C2_PreIncrement_PostIncrement.java
@@ -0,0 +1,40 @@
+package day04_increment_matematiksel_islemler;
+
+public class C2_PreIncrement_PostIncrement {
+
+	public static void main(String[] args) {
+		
+		int sayi1= 20;
+
+		int sayi2=++sayi1;
+		
+		System.out.println("pre-increment sayi1 : " + sayi1); //21
+		System.out.println("pre-increment sayi2 : " + sayi2); //21
+		
+		
+		sayi2=sayi1++;
+		
+		System.out.println("post-increment sayi1 : " + sayi1); // 22
+		System.out.println("post-increment sayi2 : " + sayi2); // 21
+		
+		
+		
+		int sayi3=10;
+		
+		System.out.println("pre increment : " + ++sayi3); // 11
+		
+		System.out.println("post increment : " + sayi3++); // 11
+		
+		System.out.println("post increment'tan sonra sayi3 : " + sayi3); // 12
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/day04_increment_matematiksel_islemler/C3_PreDecrement_PostDecrement.java
@@ -0,0 +1,5 @@
+package day04_increment_matematiksel_islemler;
+
+public class C3_PreDecrement_PostDecrement {
+
+}
